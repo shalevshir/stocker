ee2d81880272eff9b1f95dfcaa029d9b4b33c02c
shalevshir <shalevshir@gmail.com>
2025-06-04 20:25:17 +0300

    chore(env): update requirements and create_tables for environment and dependency management

--- create_tables.py
+++ create_tables.py
@@ -1,9 +1,14 @@
 from sqlalchemy import create_engine
 from models import Base
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Update the connection string as needed
-DATABASE_URL = "postgresql+psycopg2://stocker:stockerpass@localhost:54320/stockerdb"
-
+DATABASE_URL = os.environ.get("DATABASE_URL")
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL environment variable not set.")
 engine = create_engine(DATABASE_URL)
 
 # Create all tables
